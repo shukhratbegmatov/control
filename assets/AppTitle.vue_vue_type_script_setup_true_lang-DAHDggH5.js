d7a2985914889a8e00222bb3f9ddb1a390856fa0
Shuxrat <shuukhratbegmatov@gmail.com>
2024-12-17 20:50:52 +0500

    fix

--- assets/AppTitle.vue_vue_type_script_setup_true_lang-DAHDggH5.js
+++
@@ -1,1 +0,0 @@
-import{d as t,i as o,h as s,O as n,P as r,j as l,o as p,x as u,G as c,T as i,z as d}from"./index-Dpjr3xtB.js";const T=t({__name:"AppTitle",setup(f){let e=o(!1);return s(async()=>{await n(),e.value=!0}),r(()=>{e.value=!1}),(a,m)=>l(e)?(p(),u(i,{key:0,to:"#appTitle"},[c(a.$slots,"default")])):d("",!0)}});export{T as _};
--- assets/index-BTi59fAx.js
+++
@@ -1,1 +0,0 @@
-import{d as o,u as e,t as n,h as s,i as r,a as u}from"./index-Dpjr3xtB.js";const d=o({__name:"index",setup(c){const{getUser:t}=e();n(e()),s(t),r(!1);const a=u();return s(async()=>{await a.push("/docs")}),(_,f)=>null}});export{d as default};
